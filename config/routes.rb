be8f2891f61e462896b7d9e0ad3230396025cc77
kdank <danknerj@gmail.com>
2013-03-31 15:59:55 -0400

    finishing up static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Vocabulator::Application.routes.draw do
+  get "users/new"
   root to: 'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
--- spec/features/ static_pages_spec.rb
+++ spec/features/ static_pages_spec.rb
@@ -7,36 +7,8 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
-
-    describe "for signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem")
-        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
-        sign_in user
-        visit root_path
-      end
-      
-      it "should render the user's feed" do
-        user.feed.each do |item|
-          expect(page).to have_selector("li##{item.id}", text: item.content)
-        end
-      end
-
-      describe "follower/following counts" do
-        let(:other_user) { FactoryGirl.create(:user) }
-        before do
-          other_user.follow!(user)
-          visit root_path
-        end
-
-        it { should have_link("0 following", href: following_user_path(user)) }
-        it { should have_link("1 followers", href: followers_user_path(user)) }
-      end
-    end
   end
 
   describe "About page" do
