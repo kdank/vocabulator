91994e0a762cde796a61143ecf6d3e14af2aaea4
kdank <danknerj@gmail.com>
2013-04-06 17:22:32 -0400

    a bunch of stuff, user signup, signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Vocabulator::Application.routes.draw do
-  get "users/new"
+  resources :users
+  resources :sessions,      only: [:new, :create, :destroy]
   root to: 'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/jacob',   to: 'static_pages#jacob',   via: 'get'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,16 @@
-def full_title(page_title)
-  base_title = "Vocabulator"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
+include ApplicationHelper
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
